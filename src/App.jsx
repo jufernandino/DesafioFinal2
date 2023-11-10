f9502de12d0910c42313a45a79eba8a1e2ec0d3c
Júlia Vasconcelos <juliavf1109@gmail.com>
2023-11-10 09:55:56 -0300

    Implementando Simbolos

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,31 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const favoriteStyle = {
+    color: 'white',
+  };
+
   return (
     //APLICATIVO GERAL
     <div className='app'>
       {/*BARRA LATERAL*/}
       <div className='left-container'>
         <h1 className='title'>iSpotify ®</h1>
-        <button className='artist-btn'>Artistas</button>
-        <button className='like-btn'>Músicas Curtidas</button>
-        <button className='logout-btn'>Logout</button>
+        <button className='artist-btn'>
+          <span class="material-symbols-outlined">
+            radio_button_checked&ensp;</span>
+          Artistas
+        </button>
+        <button className='like-btn'>
+          <span style = {favoriteStyle} className="material-symbols-outlined">
+            favorite&ensp;</span>
+          Músicas Curtidas
+        </button>
+        <button className='logout-btn'>
+          <span class="material-symbols-outlined">
+            logout&ensp;</span>
+          Logout
+        </button>
       </div>
       {/*AREA PRINCIPAL*/}
       <div className='main-container'>
